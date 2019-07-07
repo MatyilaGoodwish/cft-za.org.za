179e47235f28f7deb7d2e64f30db946adb3d35c6
Matyila Goodwish <goodwish@axxess.co.za>
2019-07-07 17:53:12 +0200

    update

--- public/app/members/register.component.js
+++ public/app/members/register.component.js
@@ -3,9 +3,29 @@
     registerComponent.component("registrar",{
         templateUrl:"app/views/registrations.html",
         controller: function registerController (DbContextSvc){
-            
             this.saveRecord = function(){
-                
+                firebase.firestore().collection("housing").doc(this.member.idnumber).set({
+                    ref: this.member.ref || "incomplete",
+                    fullnames: this.member.fullnames || "incomplete",
+                    lastname: this.member.surname || "incomplete",
+                    title: this.member.title || "incomplete",
+                    race: this.member.race || "incomplete",
+                    application: this.member.application || "incomplete",
+                    nationality: this.member.nationality || "incomplete",
+                    adddress: this.member.address || "incomplete",
+                    province : this.member.province || "incomplete",
+                    idnumber: this.member.idnumber || "incomplete",
+                    contact: this.member.contact || "incomplete",
+                    gender: this.member.gender || "incomplete",
+                    maritalstatus: this.member.maritalstatus || "incomplete",
+                    spouse: this.member.spouse || "incomplete"
+                })
+                .then(function(){
+                    swal("Confirmation", "Record filed in the database", "success");
+                })
+                .catch(function(error){
+                   swal("Permission", error.message, "error");
+                })
             }
         }
     })
