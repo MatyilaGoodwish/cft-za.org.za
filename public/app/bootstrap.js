ab5a03a349f357ed545371a8566f1c4bf905db57
Matyila Goodwish <goodwish@axxess.co.za>
2019-07-07 18:58:16 +0200

    update querie views and common modules

--- public/app/bootstrap.js
+++ public/app/bootstrap.js
@@ -6,6 +6,42 @@
         .when("/", {
             template: "<group></group>"
         })
+        .when("/waiting", {
+            template: "<waiting></waiting>",
+            resolves: function(){
+                swal("test resolves");
+            }
+        })
+        .when("/new", {
+            template: "<new></new>",
+            resolves: function(){
+                swal("test resolves");
+            }
+        })
+        .when("/forms", {
+            template: "<forms></forms>",
+            resolves: function(){
+                swal("test resolves");
+            }
+        })
+        .when("/demand", {
+            template: "<demand></demand>",
+            resolves: function(){
+                swal("test resolves");
+            }
+        })
+        .when("/lostforms", {
+            template: "<lostforms></lostforms>",
+            resolves: function(){
+                swal("test resolves");
+            }
+        })
+        .when("/subsidy", {
+            template: "<subsidy></subsidy>",
+            resolves: function(){
+                swal("test resolves");
+            }
+        })
         .when("/register",{
             template: "<registrar></registrar>"
         })
---
+++ public/app/members/common.component.js
@@ -0,0 +1,39 @@
+(function(){
+    const CommonQueriesAppModule = angular.module("app");
+    CommonQueriesAppModule.component("waiting", {
+      templateUrl: "app/views/waiting.html",
+      controller: [function(){
+            
+      }]
+    })
+    CommonQueriesAppModule.component("new", {
+      templateUrl: "app/views/new.html",
+      controller: [function(){
+            
+      }]
+    })
+    CommonQueriesAppModule.component("subsidy", {
+      templateUrl: "app/views/subsidy.html",
+      controller: [function(){
+            
+      }]
+    })
+    CommonQueriesAppModule.component("lostforms", {
+        templateUrl: "app/views/lostforms.html",
+        controller: [function(){
+              
+        }]
+      })
+      CommonQueriesAppModule.component("forms", {
+        templateUrl: "app/views/formsc.html",
+        controller: [function(){
+              
+        }]
+      })
+      CommonQueriesAppModule.component("demand", {
+        templateUrl: "app/views/demand.html",
+        controller: [function(){
+              
+        }]
+      })
+ }())
